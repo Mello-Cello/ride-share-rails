f733bd0fa894a5a69b10531c3713ff2f2c94c803
Mello-Cello <celloelle@gmail.com>
2019-04-16 16:18:54 -0700

    Added a comment. Testing pushing a branch.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # add other routes later if needed
   resources :trips, :drivers, :passengers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
