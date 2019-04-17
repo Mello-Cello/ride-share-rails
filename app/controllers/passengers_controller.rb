956ab0e603cb05e576b80d7d3a2701512b27cad4
Mello-Cello <celloelle@gmail.com>
2019-04-16 19:08:47 -0700

    Created Passenger index view.

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,7 +1,14 @@
 class PassengersController < ApplicationController
   def index
+    @passengers = Passenger.all
+  end
+
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
   end
 
   def new
+    @passenger = Passenger.new
   end
 end
