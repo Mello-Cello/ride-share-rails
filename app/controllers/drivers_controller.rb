34d55c229b095e8c1f5d1818a75191ff4a205a2b
Mello-Cello <celloelle@gmail.com>
2019-04-16 19:23:07 -0700

    Generated the views for all passengers and drivers.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,14 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id]
+    @drivers = Driver.find_by(id: driver_id)
   end
 
   def new
+    @drivers = Driver.new
   end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,14 @@
 class TripsController < ApplicationController
   def index
+    @trips = Trip.all
+  end
+
+  def show
+    trip_id = params[:id]
+    @trips = Trip.find_by(id: trip_id)
+  end
+
+  def new
+    @trips = Trip.new
   end
 end
