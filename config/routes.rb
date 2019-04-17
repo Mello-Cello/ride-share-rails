00bfa63b770e3178564037e092a955c20fe9181d
Mello-Cello <celloelle@gmail.com>
2019-04-16 18:49:11 -0700

    Generated controllers for Drivers, Passengers and Trips.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'passengers/index'
+  get 'passengers/new'
+  get 'drivers/index'
+  get 'drivers/new'
+  get 'trips/index'
   # add other routes later if needed
   resources :trips, :drivers, :passengers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
