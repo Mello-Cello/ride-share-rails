344a595198fd9fadc40c757c7132c80eb3aef3d3
Mello-Cello <celloelle@gmail.com>
2019-04-18 15:23:32 -0700

    Added update and create actions and their corresponding views. Made private passenger_params method. Added validation to passenger model - require name and phone num.

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -15,4 +15,34 @@ class PassengersController < ApplicationController
   def edit
     @passenger = Passenger.find_by(id: params[:id])
   end
+
+  def update
+    this_passenger = Passenger.find_by(id: params[:id])
+
+    is_successful = this_passenger.update(passenger_params)
+
+    if is_successful
+      redirect_to passenger_path
+    else
+      redirect_to passenger_path
+    end
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    is_successful = @passenger.save
+
+    if is_successful
+      redirect_to passenger_path(@passenger.id)
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def passenger_params
+    params.require(:passenger).permit(:name, :phone_num)
+  end
 end
