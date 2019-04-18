73ea5b011bdb4b4d90c2bf5a178317788cfb1fbf
Mello-Cello <celloelle@gmail.com>
2019-04-18 13:45:37 -0700

    Nested routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  get 'passengers/index'
-  get 'passengers/new'
-  get 'drivers/index'
-  get 'drivers/new'
-  get 'trips/index'
+
+  # WE DIDN'T ADD THESE. COMMENTING THEM OUT UNTIL WE KNOW WE NEED THEM.
+  # get 'passengers/index'
+  # get 'passengers/new'
+  # get 'drivers/index'
+  # get 'drivers/new'
+  # get 'trips/index'
+
   # add other routes later if needed
+
+  resources :drivers do
+    resources :trips, only: [:index, :new]
+  end
+
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
+
   resources :trips, :drivers, :passengers
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
