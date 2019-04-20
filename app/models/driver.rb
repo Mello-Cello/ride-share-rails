9d9fdc4a560e7c5fa2660eb42bebf99e288c229d
Mello-Cello <celloelle@gmail.com>
2019-04-20 15:45:28 -0700

    Added methods to driver and passenger models to calculate totals spent, earned, and avg rating.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,4 +4,25 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true, uniqueness: true
+
+  def total_earned
+    sum = 0
+    self.trips.each do |trip|
+      sum += ((trip.cost.to_f / 100) - 1.65) * 0.8
+    end
+    return sum
+  end
+
+  def avg_rating
+    ratings_total = 0
+    i = 0
+    self.trips.each do |trip|
+      if trip.rating
+        ratings_total += trip.rating.to_f
+      end
+      i += 1
+    end
+    avg_rating = ratings_total / i
+    return avg_rating.round(2)
+  end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,12 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_spent
+    sum = 0
+    self.trips.each do |trip|
+      sum += trip.cost.to_f / 100
+    end
+    return sum
+  end
 end
